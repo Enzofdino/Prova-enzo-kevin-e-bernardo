8d3c2c3e13090618e3db0fbc5fb64574b9b06ea5
Enzofdino <enzofhostin@gmail.com>
2024-07-09 14:31:54 -0300

    parte 1 e 2 da classe game manager c
    criamos as variaveis e metodos

---
+++ Prova Enzo,bernardo,kevin/Assets/Scenes/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int pontos, teclaAtual;
+    float relogio;
+    KeyCode teclas;
+    UIGamemanager gamemanager;
+    public void Start()
+    {
+        Gerarsetas
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)
+        {
+          Checartecla(DownArrow)
+        }
+        if (Input.GetKeyUp(KeyCode.Escape)
+        {
+            Checartecla(UpArrow)
+        }
+        if(Input.GetKeyRightArrow) 
+        { 
+         ChecarTecla(RightArrow)
+        }
+        if (Input.GetKeyLeftArrow)
+        {
+            ChecarTecla(LeftArrow)
+        }
+        ContagemRegressiva;
+
+       
+    }
+}
